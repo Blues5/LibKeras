df2c84a573e18f7820bb0d1fca029104985a7694
Gustavo Rosa <gth.rosa@uol.com.br>
2017-03-20 10:57:03 -0400

    Added feature extraction wrapper.

---
+++ extract_features.py
@@ -0,0 +1,38 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
+import common
+import numpy as np
+from keras.applications.imagenet_utils import preprocess_input
+from keras.preprocessing import image
+from os.path import join
+
+# Default directory
+root_out = '/home/serim/src/LibKeras'
+
+# Creating and instanciating the chosen CNN
+cnet = common.ConvNet()
+cnet.build_resnet50(include_top=False, weights='imagenet', classes=1000)
+#cnet.build_vgg16(include_top=False, weights='imagenet', classes=1000)
+#cnet.build_vgg19(include_top=False, weights='imagenet', classes=1000)
+
+# Loading and pre-processing input image
+img_path = 'dog.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+print('Input image shape:', x.shape)
+
+# Extracting input image features
+features = cnet.predict(x, batch_size=1)
+
+# Concatenating to 1-D array
+features = np.squeeze(features)
+
+# Features output path
+features_path = join(root_out, 'features/' + img_path + '_features.txt');
+
+# Saving output
+np.savetxt(features_path, features, fmt='%f')
+
+import gc; gc.collect()
---
+++ pre_trained.py
@@ -0,0 +1,33 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
+import common
+import numpy as np
+from keras.applications.imagenet_utils import preprocess_input
+from keras.preprocessing import image
+from os.path import join
+
+# Default directory
+root_out = '/home/serim/src/LibKeras'
+
+# Predictions path
+predictions_path = join(root_out, 'predictions/' + 'imagenet_predictions.json');
+
+# Creating and instanciating the chosen CNN
+cnet = common.ConvNet()
+cnet.build_resnet50(include_top=True, weights='imagenet', classes=1000)
+#cnet.build_vgg16(include_top=True, weights='imagenet', classes=1000)
+#cnet.build_vgg19(include_top=True, weights='imagenet', classes=1000)
+
+# Loading and pre-processing input image
+img_path = 'dog.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+print('Input image shape:', x.shape)
+
+# Predicting input image
+preds = cnet.predict(x, batch_size=1)
+print('Predicted:', cnet.decode_predictions(preds, path=predictions_path, top=5))
+
+import gc; gc.collect()
